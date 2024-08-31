28b14c40981fb01908c098e0283b007878702f8a
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-08-31 14:28:21 +0200

    bump die versie

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='uvtrick',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=[
         'uv'
