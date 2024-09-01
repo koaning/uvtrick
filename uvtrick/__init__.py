4b60f8ab5c48e77bd53c9487982458b513a37fb6
Louis Maddox <louismmx@gmail.com>
2024-09-01 23:50:41 +0100

    refactor: use `maincall` as an OOP method (do not need to pass input/output paths)

--- uvtrick/__init__.py
+++ uvtrick/__init__.py
@@ -15,18 +15,6 @@ def argskwargs_to_callstring(func, *args, **kwargs):
     return f"{func.__name__}({string_args} {string_kwargs})"
 
 
-def maincall(func, inputs_path, outputs_path):
-    return f"""
-if __name__ == "__main__":
-    import pickle
-
-    with open('{inputs_path}', 'rb') as file:
-        args, kwargs = pickle.load(file)
-
-    with open('{outputs_path}', 'wb') as f:
-        pickle.dump({func.__name__}(*args, **kwargs), f)
-"""
-
 def uvtrick_(path, func, *args, **kwargs):
     """This is a *very* hacky way to run functions from Python files from another virtual environment."""
     string_kwargs = ", ".join([f"{k}={v}" for k, v in kwargs.items()])
@@ -120,6 +108,25 @@ class Env:
         print(f"Contents of the script:\n\n{contents}")
         return
 
+    def maincall(self, func) -> str:
+        """A main block to deserialise a function signature then serialise a result.
+
+        Load the args/kwargs from an 'inputs' pickle, call a Python function
+        with them, and store the result in an 'output' pickle.
+        """
+        func_name = func.__name__
+        inputs_path, output_path = self.inputs, self.output
+        return textwrap.dedent(f"""
+        if __name__ == "__main__":
+            import pickle
+            from pathlib import Path
+
+            args, kwargs = pickle.loads(Path('{inputs_path!s}').read_bytes())
+            result = {func_name}(*args, **kwargs)
+            Path('{output_path!s}').write_bytes(pickle.dumps(result))
+        """)
+
+
     def run(self, func, *args, **kwargs):
         """Run a function in the virtual environment using uv."""
         
@@ -130,7 +137,7 @@ class Env:
             # Now write the contents of the script
             contents = textwrap.dedent(inspect.getsource(func))
             contents += "\n\n"
-            contents += maincall(func, self.inputs, self.output)
+            contents += self.maincall(func)
             self.script.write_text(contents)
 
             if self.debug:
