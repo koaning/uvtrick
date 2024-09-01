a2919ee411d0b20d03c6a4c5fcefa477c3013152
Louis Maddox <louismmx@gmail.com>
2024-09-01 17:03:38 +0100

    refactor: put root dir on PATH in tests

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+import sys
+
+root_dir = Path(__file__).parents[1]
+sys.path.append(str(root_dir))
--- tests/example_test.py
+++ tests/example_test.py
@@ -1,12 +1,5 @@
-# from pytest import importorskip
-# from pathlib import Path
-# import sys
-# import subprocess
-# 
-# root_dir = Path(__file__).parents[1]
-# sys.path.append(str(root_dir))
-# 
-# demo = root_dir / "examples" / "demo.py"
-# 
-# def test_demo():
-#     subprocess.run(f"python {demo}", shell=True, check=True)
+from pytest import importorskip
+
+
+def test_demo():
+    importorskip("examples.demo")
