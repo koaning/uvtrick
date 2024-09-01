15189859df1650f589be195a2b2b89e65b6455d0
Louis Maddox <louismmx@gmail.com>
2024-09-01 22:51:18 +0100

    refactor: use list args for `uvtrick_` subprocess call and do not use `shell=True`; check its return code (fail fast)

--- uvtrick/__init__.py
+++ uvtrick/__init__.py
@@ -35,6 +35,8 @@ def uvtrick_(path, func, *args, **kwargs):
 
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_dir = Path(temp_dir)
+        script = temp_dir / "pytemp.py"
+
         code = Path(path).read_text()
         idx = code.find("if __name__")
         code = code[:idx] + "\n\n"
@@ -51,7 +53,8 @@ def uvtrick_(path, func, *args, **kwargs):
 
         Path(temp_dir / "pytemp.py").write_text(code)
         # print(code)
-        subprocess.run(f"uv run --quiet {str(temp_dir / 'pytemp.py')}", shell=True, cwd=temp_dir)
+        cmd = ["uv", "run", "--quiet", str(script)]
+        subprocess.run(cmd, cwd=temp_dir, check=True)
 
         temp_pickle_path = os.path.join(temp_dir, "tmp.pickle")
         with open(temp_pickle_path, 'rb') as file:
