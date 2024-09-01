d5f7359b5c281bdb9edb240aaa6f145a5cb66719
Louis Maddox <louismmx@gmail.com>
2024-09-01 22:58:53 +0100

    refactor: prepare subprocess command for Env.run and debug printing only once; do not use `shell=True`; check return code

--- uvtrick/__init__.py
+++ uvtrick/__init__.py
@@ -101,6 +101,7 @@ class Env:
         with tempfile.TemporaryDirectory() as temp_dir:
             # Lets first pickle the inputs
             temp_dir = Path(temp_dir)
+            script = temp_dir / "pytemp.py"
             with open(temp_dir / "pickled_inputs.pickle", "wb") as f:
                 pickle.dump((args, kwargs), f)
             
@@ -108,21 +109,22 @@ class Env:
             contents = textwrap.dedent(inspect.getsource(func))
             contents += "\n\n"
             contents += maincall(func, temp_dir / PICKLED_INPUTS_PATH, temp_dir / PICKLED_OUTPUTS_PATH)
-            Path(temp_dir / "pytemp.py").write_text(contents)
+            script.write_text(contents)
             
-            deps = " ".join([f"--with {dep}" for dep in self.requirements])
-            pyversion = f"--python {self.python}" if self.python else ""
-            quiet = "--quiet" if not self.debug else ""
+            quiet = [] if self.debug else ["--quiet"]
+            deps = [f"--with={dep}" for dep in self.requirements]
+            pyversion = [f"--python=self.python"] if self.python else []
+            cmd = ["uv", "run", *quiet, *deps, *pyversion, str(script)]
 
             if self.debug:
-                print(f"Running files in {temp_dir}")
-                print(f"uv run --quiet {deps} {pyversion} {str(temp_dir / 'pytemp.py')}")
+                print(f"Running files in {temp_dir}\n{cmd}")
                 with open(temp_dir / PICKLED_INPUTS_PATH, 'rb') as file:
                     args, kwargs = pickle.load(file)
                 print(f"Pickled args: {args}")
                 print(f"Pickled kwargs: {kwargs}")
                 print(f"Contents of the script:\n\n {contents}")
-            subprocess.run(f"uv run {quiet} {deps} {pyversion} {str(temp_dir / 'pytemp.py')}", shell=True, cwd=temp_dir)
+
+            subprocess.run(cmd, cwd=temp_dir, check=True)
 
             temp_pickle_path = os.path.join(temp_dir, "tmp.pickle")
             with open(temp_pickle_path, 'rb') as file:
